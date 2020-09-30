284bc0c8575e0f311a26c3ef95094fb36e04deb4
Matilda Chiang <chiang.matilda@gmail.com>
2020-09-30 13:40:46 -0700

    Initial File setup
    added css files for project

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title> IAT352 PHP Project </title>
+
+  <meta name="viewport" content="width=device-width, initial=scale=1.0"> <!-- -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/fonts.css">
+</head>
+
+<body>
+
+
 <? php
 echo "test";
 
 ?>
+
+</body>
+</html>
