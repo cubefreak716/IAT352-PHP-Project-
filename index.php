89a4e7affe895dce5aa7ab5de22226f029c73fee
Louis <dlou10@live.ca>
2020-12-02 15:00:46 -0800

    Added Favorites to Index

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <!DOCTYPE HTML>
 <?php
 session_start();
+//connection set up
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "louis_fourie";
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+//testing connection
+if(mysqli_connect_errno()){
+  die("Database connection failed: " . mysqli_connect_error() .
+  " (" . mysqli_connect_errno() . ")"
+  );
+}
  ?>
+
+
+
 <html lang="en">
 <head>
   <meta charset="utf-8">
@@ -64,7 +80,93 @@ session_start();
     <img class="index-image" src="img/Pay-Parking-t2.jpg" alt="payparkingheader">
   </div>
 
-  <div class="index-bottom box">
+<?php
+if(isset($_SESSION["ID"])){
+echo'<div class="favourite-box">
+  <h2 class=""> Favourite Paystations</h2>
+  <div class="bookmark-list">
+    <ul>';
+
+      $query_getBookmarks = "SELECT * FROM bookmarks WHERE bookmarks.ID_user = '";
+      $query_getBookmarks .= $_SESSION['ID']. "'";
+      $result = mysqli_query($connection, $query_getBookmarks);
+
+      if(!$result){
+        die("query failed");
+      }
+      else{
+
+        $isempty=0;
+        while($row = mysqli_fetch_assoc($result)){
+          echo "<li>";
+          $isempty=1;
+          //echo $row['ID_paystation'];
+          echo " ";
+          $query_getStreetName = "SELECT * FROM pay_stations WHERE pay_stations.METER_ID ='";
+          $query_getStreetName .= $row['ID_paystation']. "'";
+          $result2 = mysqli_query($connection, $query_getStreetName);
+          while($strname = mysqli_fetch_assoc($result2)){
+            echo "<a href='item.php?data=".$strname["METER_ID"]."'><div class='item'>";
+            echo "<div class='item-num'>";
+            echo $strname["METER_ID"];
+            echo "</div> ";
+            if(strpos($strname["METER_TYPE"],'Paystation') !== false){
+              echo "<img class='icon-meter' src='img/parking.png' alt='paystationicon'> ";
+            }
+            if(strpos($strname["METER_TYPE"],'EV') !== false){
+              echo "<img class='icon-meter' src='img/charging.png' alt='chargingstationicon'> ";
+            }
+            echo $strname["ADDRESS"];
+            echo " ";
+            echo "<div class='item-hourrate'> Rate: ";
+            echo $strname["HOURLY_RATE"];
+            echo "</div>";
+            echo "</div></a>";
+          }
+          echo "</li>";
+        }
+      }
+      if($isempty==0){
+        echo'<p>You Do Not Have Any Favourites. Here Are Some Suggestions:</p>';
+
+          echo "<li>";
+
+          //echo $row['ID_paystation'];
+          echo " ";
+          $query_getStreetName = "SELECT * FROM pay_stations LIMIT 0,4";
+
+          $result2 = mysqli_query($connection, $query_getStreetName);
+          while($strname = mysqli_fetch_assoc($result2)){
+            echo "<a href='item.php?data=".$strname["METER_ID"]."'><div class='item'>";
+            echo "<div class='item-num'>";
+            echo $strname["METER_ID"];
+            echo "</div> ";
+            if(strpos($strname["METER_TYPE"],'Paystation') !== false){
+              echo "<img class='icon-meter' src='img/parking.png' alt='paystationicon'> ";
+            }
+            if(strpos($strname["METER_TYPE"],'EV') !== false){
+              echo "<img class='icon-meter' src='img/charging.png' alt='chargingstationicon'> ";
+            }
+            echo $strname["ADDRESS"];
+            echo " ";
+            echo "<div class='item-hourrate'> Rate: ";
+            echo $strname["HOURLY_RATE"];
+            echo "</div>";
+            echo "</div></a>";
+          }
+          echo "</li>";
+
+      }
+    echo'
+    </ul>
+  </div>
+</div>';
+
+}
+ ?>
+
+<?php if(!isset($_SESSION["ID"])){
+  echo '<div class="index-bottom box">
     <div class="index-info">
       <div class="title">What is Paystation Finder</div>
       <p>
@@ -82,8 +184,8 @@ session_start();
         Sed ac justo nec nisi rutrum luctus. Maecenas justo sapien, ullamcorper sed malesuada feugiat, commodo ac sem. Morbi id turpis ultrices, dapibus dui eget, interdum sapien.
       </p>
     </div>
-  </div>
-
+  </div>';
+} ?>
 
 </body>
 </html>
